807610549688750e5d3da71668f77a1bb337d9d9
tcousik <119012703+tcousik@users.noreply.github.com>
2023-10-25 12:33:25 -0600

    user route

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import { userRouter } from "./src/routes/users.js";
+// import { recipesRouter } from "./routes/recipes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use("/auth", userRouter);
+// app.use("/recipes", recipesRouter);
+
 mongoose.connect(
-    "mongodb+srv://user:pass@bookstore.hqpkize.mongodb.net/recipes?retryWrites=true&w=majority"
-)
+    "mongodb+srv://user:pass@bookstore.hqpkize.mongodb.net/recipes?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
 
 app.listen(3001, () => console.log("Server started"));
