fe638950e9315512c136cd9807909f6f3b6604aa
tcousik <119012703+tcousik@users.noreply.github.com>
2023-10-25 12:27:17 -0600

    userSchema

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
+mongoose.connect(
+    "mongodb+srv://user:pass@bookstore.hqpkize.mongodb.net/recipes?retryWrites=true&w=majority"
+)
 
 app.listen(3001, () => console.log("Server started"));
